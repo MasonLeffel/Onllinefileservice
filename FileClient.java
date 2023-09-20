ae31eb02592780992795c31c5760c945572a52e6
MasonLeffel <112015017+MasonLeffel@users.noreply.github.com>
2023-09-20 13:52:50 -0400

    Add files via upload

---
+++ FileClient.java
@@ -0,0 +1,41 @@
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class FileClient {
+    public static void main(String[] args) throws Exception{
+        if(args.length !=2){
+            System.out.print("Syntax: TCPEchoClient <ServerIP> <ServerPort");
+            return;
+        }
+        while (true) {
+            int serverPort = Integer.parseInt(args[1]);
+
+            Scanner scanner = new Scanner(System.in);
+            String message = scanner.nextLine();
+            System.out.print("Enter 'uoload,''download,'rename,' or 'delete' : ");// put file defults here
+
+            SocketChannel channel = SocketChannel.open();
+            channel.connect(new InetSocketAddress(args[0], serverPort));
+
+            ByteBuffer buffer = ByteBuffer.wrap(message.getBytes());
+            // read  the buffer content and write that to TCP channel
+            channel.write(buffer);
+
+            ByteBuffer serverReply = ByteBuffer.allocate(1024);
+            int bytesRead = channel.read(serverReply);
+            if (bytesRead > 0) {
+                serverReply.flip();
+                byte[] serverMessage = new byte[bytesRead];
+                serverReply.get(serverMessage);
+                System.out.println(new String(serverMessage));
+            } else {
+                System.out.printf("Error: no respone from the server.");
+            }
+            channel.close();
+        }
+    }
+}
+
+
